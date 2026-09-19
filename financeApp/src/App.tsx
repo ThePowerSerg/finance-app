f43409541f1b26c06d84e957e44041ce3bc9f0ce
ThePowerSerg <sferreir@bu.edu>
2026-09-18 21:18:39 -0400

    base line

--- financeApp/src/App.tsx
+++ financeApp/src/App.tsx
@@ -1,44 +1,11 @@
-import { useEffect, useState } from "react";
-import type { Book } from "./models/book";
-import LearningResources from "./Components/LearningResources";
-import { Container, Typography } from "@mui/material";
-// import animation from "./assets/images/animation1.svg";
+import { Box } from "@mui/material";
 
 function App() {
-  // useState hook keeps track of state changes and accepts a variable and a function
-  const [books, setBooks] = useState<Book[]>([]);
-
-  // useEffect hook lets a component synchronize with something outside React
-  useEffect(() => {
-    fetch("http://localhost:5278/api/books")
-      .then((response) => response.json())
-      .then((data) => setBooks(data));
-  }, []);
-
-  const addBook = () => {
-    setBooks((currentBooks) => {
-      const nextId = Math.max(0, ...currentBooks.map((book) => book.id)) + 1;
-
-      return [
-        ...currentBooks,
-        { id: nextId, title: "Book new", author: "favorite book" },
-      ];
-    });
-  };
-
   return (
     <>
-      <Container>
-        <Typography variant="h4" color="primary">
-          FinAlysis
-        </Typography>
-        {/* <img
-          src={animation}
-          style={{ width: "40%", height: "40%" }}
-          alt="animation"
-        /> */}
-        <LearningResources books={books} addBook={addBook} />
-      </Container>
+      <Box sx={{ maxWidth: "xl", mx: "auto", px: 4, position: "relative" }}>
+        test
+      </Box>
     </>
   );
 }
