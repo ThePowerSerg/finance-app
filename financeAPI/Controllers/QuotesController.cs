a689de6d8dfe719c6cb8a75252291975ccb192b9
ThePowerSerg <sferreir@bu.edu>
2026-09-19 20:25:39 -0400

    base get

--- financeAPI/Controllers/QuotesController.cs
+++ financeAPI/Controllers/QuotesController.cs
@@ -2,6 +2,7 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using financeAPI.Models;
 using financeAPI.Services;
 using Microsoft.AspNetCore.Mvc;
 
@@ -11,19 +12,21 @@ namespace financeAPI.Controllers
     [Route("api/[controller]")]
     public class QuotesController : ControllerBase
     {
-        private readonly IQuoteService _quoteService;
         private readonly HttpClient _httpClient;
 
-        public QuotesController(IQuoteService quoteService, HttpClient httpClient)
+        public QuotesController(HttpClient httpClient)
         {
-            _quoteService = quoteService;
             _httpClient = httpClient;
         }
 
-        [HttpGet("{symbol}")]
-        public async Task<IActionResult> GetQuote(string symbol)
+        [HttpGet()]
+        public async Task<IActionResult> GetQuote()
         {
-            var quote = await _quoteService.GetQuoteAsync(symbol);
+            string QUERY_URL = $"https://api.twelvedata.com/quote?symbol=AAPL&apikey=9b88c5eedb7f440b911855fa14635ebc";
+
+            Uri queryUri = new Uri(QUERY_URL);
+            var quote = await _httpClient.GetFromJsonAsync<Quote>(queryUri);
+
             if (quote == null)
             {
                 return NotFound();
